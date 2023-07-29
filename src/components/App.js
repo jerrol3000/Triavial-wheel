977c6a05d02ec91959a82b61352201ad8c75ccce
Jerral Graham <jerralgraham@yahoo.com>
2023-07-28 22:23:36 -0400

    background image removed

--- src/components/App.js
+++ src/components/App.js
@@ -18,16 +18,16 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <Paper
         className="app-container"
-        style={{
-          backgroundImage:
-            "url(https://images.unsplash.com/photo-1495195129352-aeb325a55b65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80)",
-          backgroundSize: "contain",
-          backgroundPosition: "center top 4.25em",
-          backgroundRepeat: "no-repeat",
-          height: "100vh",
-          margin: 0,
-          padding: 0,
-        }}
+        // style={{
+        //   backgroundImage:
+        //     "url(https://images.unsplash.com/photo-1495195129352-aeb325a55b65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80)",
+        //   backgroundSize: "contain",
+        //   backgroundPosition: "center top 4.25em",
+        //   backgroundRepeat: "no-repeat",
+        //   height: "100vh",
+        //   margin: 0,
+        //   padding: 0,
+        // }}
       >
         <Banner toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
         <WheelComponent />
